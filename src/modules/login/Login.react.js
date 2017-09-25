aac8a5ce4857a80b01d0c7b04f6fe9d503627ddb
liuliyuan520 <2541337855@qq.com>
2017-09-25 18:37:56 +0800

    修改路由结构和增加招标信息

--- src/modules/login/Login.react.js
+++ src/modules/login/Login.react.js
@@ -16,12 +16,12 @@ class Login extends Component{
 
     fetch = () => {
 
-        /*const appName = getUrlParam('appName') || 'xyj',
-              token = getUrlParam('token') || 'R311nqHij/l8vllu7pE2McwMg8lniQMpFnT3fT8wTMbrdaRqoR4l9sxGnwrdp4chhCC3SZM8r1A8JRA5HHpbwZrRobnMXyCbCzNjLajr4LDfZbt8TLmdyvph6YX/HmlEvNPGxn8B27xrDly8C2zLE/jwHbH/IDZ8ILpRk5ZViVw=';
-*/
-        const appName = 'admin',
-            token = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJhdWQiOiJQQ-e9kemhtSIsInN1YiI6IntcInRva2VuXCI6XCIxOTQyMTcxYjkzMmQ0MmQ3YTIzNDE2ZGY1YThlMjhmMVwiLFwiY2xpZW50SWRcIjpcInN1cHBsaWVyXCIsXCJ1c2VySWRcIjpcIjExMVwiLFwiZ3JhbnRUeXBlXCI6XCJwYXNzd29yZFwiLFwidG9rZW5UeXBlXCI6XCJiZWFyZXJcIixcInNjb3BlXCI6XCJzdXBwbGllclwiLFwiY2xpZW50U291cmNlXCI6MSxcImV4cGlyZXNJblwiOjcyMDAsXCJ0b2tlbkV4cGlyZWREYXRlXCI6MTUwMzY0ODU0MX0iLCJpc3MiOiJzZXJ2aW5nY2xvdWQiLCJleHAiOjE1MDM2NDg1NDF9.Xw1VnCuovWBDXFvvDCo6f9OcfC9z3oI1jbIu6O0iBvZj6-F8iQPLAcFIW3-6xAmmUU06HKgjsoEEZcLOH8ydVYEz0hGhv65pVm-AiiYptx2hObHFO2LNAlQfsx3gqER6UyA-rGeU7lOLiQAFhIAKCXmDeSFpuozJ6DMr0enCAIY';
+        const appName = getUrlParam('appName'),
+              token = getUrlParam('token');
 
+       /* const appName = 'admin',
+            token = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJhdWQiOiJQQ-e9kemhtSIsInN1YiI6IntcInRva2VuXCI6XCIxOTQyMTcxYjkzMmQ0MmQ3YTIzNDE2ZGY1YThlMjhmMVwiLFwiY2xpZW50SWRcIjpcInN1cHBsaWVyXCIsXCJ1c2VySWRcIjpcIjExMVwiLFwiZ3JhbnRUeXBlXCI6XCJwYXNzd29yZFwiLFwidG9rZW5UeXBlXCI6XCJiZWFyZXJcIixcInNjb3BlXCI6XCJzdXBwbGllclwiLFwiY2xpZW50U291cmNlXCI6MSxcImV4cGlyZXNJblwiOjcyMDAsXCJ0b2tlbkV4cGlyZWREYXRlXCI6MTUwMzY0ODU0MX0iLCJpc3MiOiJzZXJ2aW5nY2xvdWQiLCJleHAiOjE1MDM2NDg1NDF9.Xw1VnCuovWBDXFvvDCo6f9OcfC9z3oI1jbIu6O0iBvZj6-F8iQPLAcFIW3-6xAmmUU06HKgjsoEEZcLOH8ydVYEz0hGhv65pVm-AiiYptx2hObHFO2LNAlQfsx3gqER6UyA-rGeU7lOLiQAFhIAKCXmDeSFpuozJ6DMr0enCAIY';
+*/
         this.mounted && this.setState({ refLoading: true });
 
         oauth.setToken(token);
